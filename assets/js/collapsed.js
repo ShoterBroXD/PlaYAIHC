3c96b576a716fad7e3385b2dec3d563d80025687
Aldair Rivas Pinto <pierorivas22@gmail.com>
2025-06-10 00:50:11 -0500

    Inclusion de listas de canciones y comentarios

--- assets/js/collapsed.js
+++ assets/js/collapsed.js
@@ -1,34 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggle-sidebar');
-    const sidebar = document.querySelector('aside'); // Selecciona la sidebar
+    const sidebar = document.querySelector('aside');
+
     toggleButton.addEventListener('click', function() {
-        sidebar.classList.toggle('collapsed'); // Alterna la clase 'collapsed'
-        // Cambia el texto del botón según el estado
+        sidebar.classList.toggle('collapsed');
         if (sidebar.classList.contains('collapsed')) {
-            toggleButton.querySelector('.button-text').textContent = 'Desplegar'; // Cambia el texto a "Desplegar"
+            toggleButton.querySelector('.button-text').textContent = 'Desplegar';
         } else {
-            toggleButton.querySelector('.button-text').textContent = 'Plegar'; // Cambia el texto a "Plegar"
+            toggleButton.querySelector('.button-text').textContent = 'Plegar';
         }
     });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    const footer = document.querySelector('footer');
+    //const footer = document.querySelector('footer');
+    const footer = document.getElementById('collapsed-buttom');
     const imageContainer = document.getElementById('image-container');
-    const songList = document.querySelector('.song-list');
+    const songList = document.querySelector('.container-playlist');
     const commentList = document.querySelector('.comment-list');
 
-    footer.addEventListener('click', function() {
-        imageContainer.classList.toggle('hidden'); // Muestra u oculta la imagen
-
-        // Si la imagen se oculta, también oculta las listas
+    footer.addEventListener('click', function(event) {
+        imageContainer.classList.toggle('hidden');
+        if (event.target.id === 'Play-list') {
+            showSongList(); // Mostrar lista de canciones
+        } else if (event.target.id === 'Comment-list') {
+            showCommentList(); // Mostrar lista de comentarios
+        }
         if (imageContainer.classList.contains('hidden')) {
             songList.classList.add('hidden');
             commentList.classList.add('hidden');
         }
     });
 
-    // Funciones para mostrar listas
     function showSongList() {
         songList.classList.remove('hidden');
         commentList.classList.add('hidden');
